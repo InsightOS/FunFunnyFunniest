f20a928ff0af706bd6b7f0c46fe6f9310efe8197
InsightOS <89692848+InsightOS@users.noreply.github.com>
2021-11-20 10:45:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord #<-- Importing discord.
+from discord.ext import commands #<-- importing discord.ext for commands.
+from discord.commands import slash_command #<-- import slash commands
+
+client = commands.Bot() #<-- Defining 'client' as the bot
+
+@client.event #<-- checks for an event
+async def on_ready(): <-- Checks when the bot is ready
+  print("The bot is ready!") <-- prints "Bot is ready" when the statement falls true.
+  
+  
+client.run('TOKEN') --< runs the token
