dd7c1be30c7a8cd290daa4d96bfaac7f9084fe76
InsightOS <89692848+InsightOS@users.noreply.github.com>
2021-11-20 10:46:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from discord.commands import slash_command #<-- import slash commands
 client = commands.Bot() #<-- Defining 'client' as the bot
 
 @client.event #<-- checks for an event
-async def on_ready(): <-- Checks when the bot is ready
-  print("The bot is ready!") <-- prints "Bot is ready" when the statement falls true.
+async def on_ready(): #<-- Checks when the bot is ready
+  print("The bot is ready!") #<-- prints "Bot is ready" when the statement falls true.
   
   
-client.run('TOKEN') --< runs the token
+client.run('TOKEN') #<-- runs the token
